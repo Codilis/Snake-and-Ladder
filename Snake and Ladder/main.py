94a96be960349b9142b3d5af5909ce07a58dce33
Palash Jain <palash.j22@gmail.com>
2018-02-04 11:41:02 +0530

    Main function of base version prepared

--- Snake and Ladder/main.py
+++ Snake and Ladder/main.py
@@ -1,16 +1,11 @@
 from userInterface import *
-
-class BounceController(object):
-    def __init__(self):
-        master = Tk()
-        master.title("Snake and Ladder")
-        master.geometry("850x600")
-        img = PhotoImage( file = "lenna.gif")
-        name = ['n','s']
-        Display(master,img,name)
-        master.mainloop()
-
+        
 def main():
-    BounceController()
+    master = Tk()
+    master.title("Snake and Ladder")
+    master.geometry("850x600")
+    img = PhotoImage( file = "lenna.gif")
+    x = Display(master,img)
+    master.mainloop()
 
 main()
